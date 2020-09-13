ae555aa2897131d1b5ec2df7f2e9f7e8f6d4da77
fheller1 <heller.frederik@googlemail.com>
2020-09-13 22:17:36 +0200

    Âdded controlling and stepping with Keyboard

--- src/ui/UiMain.java
+++ src/ui/UiMain.java
@@ -1,8 +1,13 @@
 package ui;
 
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+
+import javax.swing.AbstractAction;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -22,10 +27,15 @@ public class UiMain {
 		GameBoard[] gb = new GameBoard[1];
 		JButton start = new JButton("Start!");
 		JButton step = new JButton("Step");
+		step.setVisible(false);
 		JButton up = new JButton("^");
+		up.setVisible(false);
 		JButton left = new JButton("<");
+		left.setVisible(false);;
 		JButton down = new JButton("_");
+		down.setVisible(false);
 		JButton right = new JButton(">");
+		right.setVisible(false);
 		
 		start.addActionListener(new StartButtonListener(frame, imgLabel, sizeSlider, step, start, gb));
 		step.addActionListener(new StepButtonListener(frame, imgLabel, step, start, gb));
@@ -41,7 +51,7 @@ public class UiMain {
 		
 		ImageIcon img = new ImageIcon("img/title.jpg");
 		imgLabel.setIcon(img);
-
+		
 		panel.add(sizeSlider);
 		panel.add(start);
 		panel.add(step);
@@ -52,15 +62,38 @@ public class UiMain {
 		panel.add(right);
 		sizeSlider.setEnabled(false);
 		
-		step.getInputMap().put(KeyStroke.getKeyStroke('W'), "upKey");
-		step.getInputMap().put(KeyStroke.getKeyStroke('A'), "leftKey");
-		imgLabel.getInputMap().put(KeyStroke.getKeyStroke('S'), "downKey");
-		imgLabel.getInputMap().put(KeyStroke.getKeyStroke('D'), "rightKey");
-		
-		step.getActionMap().put("upKey", new ControlKeyAction(gb, 0));
-		step.getActionMap().put("leftKey", new ControlKeyAction(gb, 3));
-		imgLabel.getActionMap().put("downKey", new ControlKeyAction(gb, 2));
-		imgLabel.getActionMap().put("rightKey", new ControlKeyAction(gb, 1));
+		panel.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_W, 0), "forward");
+        panel.getActionMap().put("forward", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(gb[0] != null) gb[0].setCurrentDirection(0);
+                step.doClick();
+            }
+        });
+        panel.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_A, 0), "left");
+        panel.getActionMap().put("left", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            	if(gb[0] != null) gb[0].setCurrentDirection(3);
+                step.doClick();
+            }
+        });
+        panel.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_S, 0), "backward");
+        panel.getActionMap().put("backward", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            	if(gb[0] != null) gb[0].setCurrentDirection(2);
+                step.doClick();
+            }
+        });
+        panel.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_D, 0), "right");
+        panel.getActionMap().put("right", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            	if(gb[0] != null) gb[0].setCurrentDirection(1);
+                step.doClick();
+            }
+        });
 		
 		frame.add(panel);
 		
